98fc8866a8efe616ba90c06494b7173f52ebab97
ZRELLIS09 <zack_ellis09@yahoo.com>
2026-05-20 11:58:50 -0500

    Add files via upload

---
+++ placeholder.js
@@ -0,0 +1,45 @@
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 240 240" width="110" height="110">
+  <defs>
+    <path id="topArc" d="M 44 120 A 76 76 0 0 1 196 120" fill="none"></path>
+    <path id="botArc" d="M 48 120 A 72 72 0 0 0 192 120" fill="none"></path>
+  </defs>
+
+  <g>
+    
+    <path d="M120.00 8.00 L130.00 18.49 L141.85 10.15 L149.61 22.39 L162.86 16.53 L168.08 30.04 L182.22 26.88 L184.71 41.15 L199.20 40.80 L198.85 55.29 L213.12 57.78 L209.96 71.92 L223.47 77.14 L217.61 90.39 L229.85 98.15 L221.51 110.00 L232.00 120.00 L221.51 130.00 L229.85 141.85 L217.61 149.61 L223.47 162.86 L209.96 168.08 L213.12 182.22 L198.85 184.71 L199.20 199.20 L184.71 198.85 L182.22 213.12 L168.08 209.96 L162.86 223.47 L149.61 217.61 L141.85 229.85 L130.00 221.51 L120.00 232.00 L110.00 221.51 L98.15 229.85 L90.39 217.61 L77.14 223.47 L71.92 209.96 L57.78 213.12 L55.29 198.85 L40.80 199.20 L41.15 184.71 L26.88 182.22 L30.04 168.08 L16.53 162.86 L22.39 149.61 L10.15 141.85 L18.49 130.00 L8.00 120.00 L18.49 110.00 L10.15 98.15 L22.39 90.39 L16.53 77.14 L30.04 71.92 L26.88 57.78 L41.15 55.29 L40.80 40.80 L55.29 41.15 L57.78 26.88 L71.92 30.04 L77.14 16.53 L90.39 22.39 L98.15 10.15 L110.00 18.49 Z M120 30 A90 90 0 1 0 120 210 A90 90 0 1 0 120 30 Z" fill="#3E6B38" fill-rule="evenodd"></path>
+    
+    <circle cx="120" cy="120" r="86" fill="none" stroke="#3E6B38" stroke-width="2.5"></circle>
+    <circle cx="120" cy="120" r="80" fill="none" stroke="#3E6B38" stroke-width="1.4"></circle>
+
+    
+    <text font-family="Impact, &#39;Bebas Neue&#39;, Arial Black, sans-serif" font-size="22" font-weight="900" fill="#3E6B38" letter-spacing="5">
+      <textPath href="#topArc" startOffset="50%" text-anchor="middle">IMPORT</textPath>
+    </text>
+    
+    <text font-family="Impact, &#39;Bebas Neue&#39;, Arial Black, sans-serif" font-size="18" font-weight="900" fill="#3E6B38" letter-spacing="4">
+      <textPath href="#botArc" startOffset="50%" text-anchor="middle">IMPORT</textPath>
+    </text>
+
+    
+    <g fill="#3E6B38">
+      <g transform="translate(120,72)"><polygon points="0,-5 1.5,-1.5 5,-1.5 2,1 3,5 0,2.5 -3,5 -2,1 -5,-1.5 -1.5,-1.5"></polygon></g>
+      <g transform="translate(104,76)"><polygon points="0,-3.5 1,-1 3.5,-1 1.4,0.7 2.1,3.5 0,1.75 -2.1,3.5 -1.4,0.7 -3.5,-1 -1,-1"></polygon></g>
+      <g transform="translate(136,76)"><polygon points="0,-3.5 1,-1 3.5,-1 1.4,0.7 2.1,3.5 0,1.75 -2.1,3.5 -1.4,0.7 -3.5,-1 -1,-1"></polygon></g>
+      <g transform="translate(120,168)"><polygon points="0,-5 1.5,-1.5 5,-1.5 2,1 3,5 0,2.5 -3,5 -2,1 -5,-1.5 -1.5,-1.5"></polygon></g>
+      <g transform="translate(104,164)"><polygon points="0,-3.5 1,-1 3.5,-1 1.4,0.7 2.1,3.5 0,1.75 -2.1,3.5 -1.4,0.7 -3.5,-1 -1,-1"></polygon></g>
+      <g transform="translate(136,164)"><polygon points="0,-3.5 1,-1 3.5,-1 1.4,0.7 2.1,3.5 0,1.75 -2.1,3.5 -1.4,0.7 -3.5,-1 -1,-1"></polygon></g>
+    </g>
+
+    
+    <polygon points="8,100 38,96 38,132 8,136 16,118" fill="#2D4A28"></polygon>
+    <polygon points="20,104 38,102 38,126 20,130 26,118" fill="#3E6B38"></polygon>
+    <polygon points="232,100 202,96 202,132 232,136 224,118" fill="#2D4A28"></polygon>
+    <polygon points="220,104 202,102 202,126 220,130 214,118" fill="#3E6B38"></polygon>
+    
+    <polygon points="36,92 204,92 214,118 204,134 36,134 46,118" fill="#3E6B38"></polygon>
+    <polygon points="40,96 200,96 209,118 200,130 40,130 50,118" fill="none" stroke="#2D4A28" stroke-width="1" opacity="0.5"></polygon>
+
+    
+    <text x="120" y="124" font-family="Impact, &#39;Bebas Neue&#39;, Arial Black, sans-serif" font-size="30" font-weight="900" fill="#ffffff" text-anchor="middle" letter-spacing="4">IMPORT</text>
+  </g>
+</svg>
